83d1cb6b9f42701c1436e2015b8233fd6c0fe82c
sgript <s94ahmad@gmail.com>
2017-09-12 01:20:30 +0100

    Authentication updates: - Authentication cleaned up, improved with SHA3 hashing. - Suspicious UUIDs trying to eavesdrop being added to auth_blacklisted - New subscriptions (channels, user's uuid) being added to database   - This will help database recover from crashes and re-subscribe on startup (still need to write).

--- client_auth.py
+++ client_auth.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from pubnub.callbacks import SubscribeCallback
 from pubnub.enums import PNStatusCategory
 from pubnub.pnconfiguration import PNConfiguration, PNReconnectionPolicy
@@ -6,7 +8,7 @@ from pubnub.pubnub import PubNub
 pnconfig = PNConfiguration()
 
 def init(auth):
-    pnconfig.uuid = 'thissACha_--31eslledddlls--en2991eew7pw012'
+    pnconfig.uuid = 'thissACdsssdddididii3eieieidodo--sodsososwososdossosop'
     pnconfig.auth_key = auth
     pnconfig.publish_key = 'pub-c-85d5e576-5d92-48b0-af83-b47a7f21739f'
     pnconfig.subscribe_key = 'sub-c-12c2dd92-860f-11e7-8979-5e3a640e5579'
@@ -20,100 +22,58 @@ def init(auth):
 
     return pubnub
 
-
 def my_publish_callback(envelope, status):
-    # Check whether request successfully completed or not
     if not status.is_error():
-        pass  # Message successfully published to specified channel.
+        print("Client: Message successfully sent to gateway.")
     else:
-        pass  # Handle message publish error. Check 'category' property to find out possible issue
-        # because of which request did fail
-        # Request can be resent using: [status retry];
-
+        print("Client: Error transmitting message to gateway.")
 
 
 class MySubscribeCallback(SubscribeCallback):
     authed = False
+    def __init__(self):
+        self.uuid_hash = hashlib.new("sha3_512")
+        encode = self.uuid_hash.update((pnconfig.uuid).encode("UTF-8"))
 
     def auth(self, auth_info):
 
-        print("auth info: " + str(auth_info))
+        print("Auth info: " + str(auth_info))
 
+        # # # Save both of these to file for client for proof-of-concept
         authkey = auth_info['auth_key']
         channel = auth_info['channel']
+        # # #
 
-        # Put this into a single method.
+        # We now use our auth key for private channel.
         pubnub = init(authkey)
 
         pubnub.add_listener(MySubscribeCallback())
-
+        print("Client Connecting to private channel {}..".format(channel))
         pubnub.subscribe().channels(channel).execute()
-
         self.authed = True
 
     def presence(self, pubnub, presence):
-        #print(presence.channel)
-        pass  # handle incoming presence data
+        pass  # Nothing to do for client.
 
     def status(self, pubnub, status):
         if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
-            pass  # This event happens when radio / connectivity is lost
+            print("Client: Unexpectedly disconnected.")
 
         elif status.category == PNStatusCategory.PNConnectedCategory:
-            # Connect event. You can do stuff like publish, and know you'll get it.
-            # Or just use the connected event to confirm you are subscribed for
-            # UI / internal notifications, etc
-            pass
+            print("Client: Connected.")
+
         elif status.category == PNStatusCategory.PNReconnectedCategory:
-            pass
-            # Happens as part of our regular operation. This event happens when
-            # radio / connectivity is lost, then regained.
-        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
-            pass
-            # Handle message decryption error. Probably client configured to
-            # encrypt messages and on live data feed it received plain text.
+            print("Client: Reconnected.")
 
     def message(self, pubnub, message):
-        if message.message == pnconfig.uuid:
+        if message.message == self.uuid_hash.hexdigest():
+            print("Client: Connecting to UUID channel to retrieve private channel information..")
             pubnub.subscribe().channels(pnconfig.uuid).execute()
         if 'auth_key' in str(message.message) and not self.authed:
-
             self.auth(message.message)
 
-        print(message.message)
-        pass  # Handle new message stored in message.message
-
-
-
-
-# # #
 if __name__ == "__main__":
     pubnub = init('')
     pubnub.add_listener(MySubscribeCallback())
+    print("Client: Connecting to gateway to be authenticated..")
     pubnub.subscribe().channels(["gateway_auth"]).execute()
-
-# result = my_listener.wait_for_message_on("gateway_auth")
-# print(result.message)
-
-#pubnub.subscribe().channels(pnconfig.uuid).execute()
-# result = my_listener.wait_for_message_on(pnconfig.uuid)
-# print(result.message)
-
-#pubnub.unsubscribe_all();
-# pubnub.remove_listener(my_listener)
-
-# my_listener = SubscribeListener()
-# pubnub.add_listener(my_listener)
-
-# print(type(result.message))
-# authkey = result.message['auth_key']
-# channel = result.message['channel']
-
-# print(authkey, channel)
-# pnconfig.auth_key = authkey
-# pnconfig.subscribe_key = 'sub-c-12c2dd92-860f-11e7-8979-5e3a640e5579'
-# pubnub = PubNub(pnconfig)
-# pubnub.subscribe().channels(channel).execute()
-# my_listener.wait_for_connect()
-# result = my_listener.wait_for_message_on(channel)
-# print('\n\n\n\n\n\n\n\n' + result.message)
--- gateway_auth.py
+++ gateway_auth.py
@@ -1,9 +1,10 @@
-# # # Authentication code for clients connecting to gateway via pubnub
+'''
+__author__ = "sgript"
 
-# TODO: However, first somehow check how many people are on the channel?
-# TODO: Enable Access Manager again, perhaps do a READ-ONLY gateway_auth channel -- so no messages sent here_now
-# TODO: Maybe need a way to destroy channel after a leave event or a timeout event (UUID channel no longer in use..)?
+Authentication code for clients connecting to gateway via pubnub
+'''
 # TODO: Implement some channel for global announcements to all devices
+# TODO: Channel groups?
 # .
 # .
 # TODO: Clean up code.
@@ -14,6 +15,8 @@ import string
 import random
 import json
 import time
+import gateway_database
+import hashlib
 
 import logging
 import pubnub
@@ -22,13 +25,9 @@ from pubnub.enums import PNStatusCategory
 from pubnub.pnconfiguration import PNConfiguration, PNReconnectionPolicy
 from pubnub.pubnub import PubNub
 
-pubnub.set_stream_logger('pubnub', logging.DEBUG)
+#pubnub.set_stream_logger('pubnub', logging.DEBUG) # Verbose, need only when required.
 pnconfig = PNConfiguration()
 
-# Keys - Perhaps move somewhere remote
-# pnconfig.subscribe_key = 'sub-c-f420fbf8-860e-11e7-9bef-b2d410151acd'
-# pnconfig.publish_key = 'pub-c-06728f1a-f12a-45b9-a501-70e1beeb88d1'
-# pnconfig.secret_key = 'sec-c-MmVjMzllOTQtNGQyMC00M2M5LWE5OGMtOWU2YTM3Mzk1MmQ3'
 pnconfig.subscribe_key = 'sub-c-12c2dd92-860f-11e7-8979-5e3a640e5579'
 pnconfig.publish_key = 'pub-c-85d5e576-5d92-48b0-af83-b47a7f21739f'
 pnconfig.secret_key = 'sec-c-YmZlMzkyYTctZDg1NC00ZTY0LWE3YzctNTkzOGRjZjk0OTI5'
@@ -44,85 +43,87 @@ pubnub = PubNub(pnconfig)
 def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-def my_publish_callback(envelope, status): # More info on publish callback -> https://www.pubnub.com/docs/python/pubnub-python-sdk
+def my_publish_callback(envelope, status):
     if not status.is_error():
-        pass  # Message successfully published to specified channel.
+        print("GatewayAuth: Message successfully sent to client.")
     else:
-        pass  # Handle message publish error. Check 'category' property to find out possible issue
+        print("GatewayAuth: Error transmitting message to client.")
 
 class MySubscribeCallback(SubscribeCallback):
+
+    def __init__(self):
+        password = input("Database password: ")
+        host = 'ephesus.cs.cf.ac.uk'
+        user = 'c1312433'
+        database = 'c1312433'
+
+        print("GatewayAuth: Starting gateway database..")
+        self.gd = gateway_database.GatewayDatabase(host, user, password, database)
+        self.sha3_hash = hashlib.new("sha3_512")
+
     def presence(self, pubnub, presence):
         if presence.channel == "gateway_auth":
-            print("\n\n\nUUID: {}\n\n\n".format(presence.uuid))
+
+            print("[1] UUID JOINED AUTH CHANNEL: {}".format(presence.uuid))
 
             pubnub.grant().channels(presence.uuid).read(True).write(True).sync()
+            print("GatewayAuth: Connecting the UUID channel {}".format(presence.uuid))
             pubnub.subscribe().channels(presence.uuid).with_presence().execute()
 
-            pubnub.publish().channel('gateway_auth').message(presence.uuid).async(my_publish_callback)
+            # Send hashed UUID over the gateway, client will know its UUID so it can hash and compute it.
+            encode = self.sha3_hash.update((presence.uuid).encode("UTF-8"))
+            pubnub.publish().channel('gateway_auth').message(self.sha3_hash.hexdigest()).async(my_publish_callback)
 
             # Check UUID channel info
             envelope = pubnub.here_now().channels(presence.uuid).include_uuids(True).include_state(True).sync()
             users_in_channel = envelope.result.total_occupancy
 
-            # Someone could be spying
+            # Someone could be spying - at this point only the gateway should be here.
             if users_in_channel > 1:
+                print("WARNING! Someone may be spying in the channel")
                 uuids_in_channel = []
                 users = envelope.result.channels[0].occupants
 
                 for occupant in users: # If there is indeed multiple people in the channel only then we bother to check who.
                     print(occupant.uuid) # - lists all uuids in channel, if more than one can later 'blacklist' ones not meant to be in the channel -> "do not serve, suspicious"
-                    uuids_in_channel.append(occupant.uuid)
 
-                if presence.uuid in uuids_in_channel:
-                    uuids_in_channel.remove(presence.uuid)
-
-                    # TODO: Save somewhere locally the suspicious UUID to read back in later - Need condition at the top to check then if this is the UUID of the user trying to auth to deny them.
+                    if occupant.uuid != "gateway" and occupant.uuid != presence.uuid: # only blacklist if not gateway or the legit user
+                        uuids_in_channel.append(occupant.uuid)
+                        self.gd.auth_blacklist(presence.channel, occupant.uuid) # blacklist them
 
                 pubnub.publish().channel(presence.uuid).message(
-                    {"error": "Too many occupants in channel, regenerate UUID."}).async(my_publish_callback) #
+                    {"error": "Too many occupants in channel, regenerate UUID."}).async(my_publish_callback)
 
-            # Nothing to do here.
-            elif users_in_channel < 1:
-                print("\n\nInternal error: no users in the UUID channel: {}".format(presence.uuid))
-                pass
-
-        elif presence.channel != "gateway_auth" and presence.uuid == presence.channel:
-            print('\n\n\nOK USER IS IN UUID CHANNEL\n\n\n')
+        elif presence.channel != "gateway_auth" and presence.uuid == presence.channel: # uuid channel presence
+            print('[2] REQUIRED USER ({}) HAS JOINED THE UUID CHANNEL ({}).'.format(presence.uuid, presence.channel))
             authkey = id_generator()
             channelName = id_generator()
 
-            # Add channel to group, making it easier to access/list later.
-            # pubnub.add_channel_to_channel_group().channels(
-            #     presence.uuid).channel_group("comm_channels").sync()
-
-            # Only need to send auth key (because permissions will change) and pub key (as they will only be supplied to subscribe key to begin with)
+            # Send auth key to user
             pubnub.publish().channel(presence.uuid).message(
-                {"channel": channelName, "auth_key": authkey, "pub_key": pnconfig.publish_key}).async(my_publish_callback) # Send data over 1-1 channel
+                {"channel": channelName, "auth_key": authkey}).async(my_publish_callback) # Send data over 1-1 channel
 
-            pubnub.unsubscribe().channels(presence.uuid).execute()
-            #pubnub.grant().channels(channelName).read(False).write(False).manage(True).sync()
+            pubnub.unsubscribe().channels(presence.uuid).execute() # no need to be subscribed to uuid channel any longer
+
+            # Grant + Subscribe to new private channel.
             pubnub.grant().channels([channelName, "policy"]).auth_keys(authkey).read(True).write(True).manage(True).ttl(0).sync()
-            # pnconfig.auth_key = authkey
-            # pubnub = PubNub(pnconfig)
+            print("GatewayAuth: Connecting to private channel {}..".format(channelName))
             pubnub.subscribe().channels(channelName).execute()
+            self.gd.gateway_subscriptions(channelName, presence.uuid)
 
-        elif presence.channel != "gateway_auth" and presence.uuid != presence.channel:
-            #msg = "This is the {} channel".format(presence.channel)
-            #pubnub.publish().channel(presence.channel).message(msg).async(my_publish_callback)
-            pass
+        elif presence.channel != "gateway_auth" and presence.uuid != presence.channel and presence.uuid != "gateway":
+            print('2. UNAUTHORISED USER ({}) HAS JOINED THE UUID CHANNEL ({}).\n\n\n'.format(presence.uuid, presence.channel))
+            self.gd.auth_blacklist(presence.channel, presence.uuid)
 
-    def status(self, pubnub, status): # More info on categories -> https://www.pubnub.com/docs/python/pubnub-python-sdk
+    def status(self, pubnub, status):
         if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
-            pass
+            print("GatewayAuth: Unexpectedly disconnected.")
 
         elif status.category == PNStatusCategory.PNConnectedCategory:
-            pass
+            print("GatewayAuth: Connected.")
 
         elif status.category == PNStatusCategory.PNReconnectedCategory:
-            pass
-
-        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
-            pass
+            print("GatewayAuth: Reconnected.")
 
     def message(self, pubnub, message):
         pass  # Handle new message stored in message.message
@@ -134,8 +135,5 @@ if __name__ == "__main__":
     pubnub.add_listener(listener)
 
     pubnub.grant().channels(["gateway_auth"]).read(True).write(True).manage(True).sync()
+    print("GatewayAuth: Connecting to gateway channel..")
     pubnub.subscribe().channels(["gateway_auth"]).with_presence().execute()
-
-# pnconfig.auth_key = 'UJARR4SCD1'
-# pubnub = PubNub(pnconfig)
-# pubnub.subscribe().channels(["E0BXKLR4T9"]).with_presence().execute()
